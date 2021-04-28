d35a6a924bb8ef1c51c85d88bb288f3855f7e624
junghwon <46914368+junghwon@users.noreply.github.com>
2021-04-28 13:38:04 +0900

    記載ミス修正

--- interfaceSample.h
+++ interfaceSample.h
@@ -2,8 +2,6 @@
 #define INTERFACE_SAMPLE_H
 
 typedef struct _interfaceSample interfaceSample_t;
-
-extern const interfaceSample_t interfaceSampleInitValue;
 typedef void (*funcManager)(interfaceSample_t *this);
 
 #endif // INTERFACE_SAMPLE_H
--- interfaceSampleClass.h
+++ interfaceSampleClass.h
@@ -7,6 +7,7 @@ struct _interfaceSample {
     funcManager pManager;
 };
 
+extern const interfaceSample_t interfaceSampleInitValue;
 void interfaceSample_Constructor (interfaceSample_t *this, interfaceSample_t initValue);
 
 #endif // INTERFACE_CLASS_SAMPLE_H
